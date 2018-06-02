e433c462fc65032135fd01bc3061d2e6d31babf2
Max Zheng <maxzheng.os@gmail.com>
2018-06-02 05:09:18 +0000

    Pin ansible

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
 
     entry_points={
        'autopip': [
-            'ansible = 2',
+            'ansible = 2.5.4',
             'ansible-hostmanager = latest',
             'twine = 1',
             'workspace-tools = latest',
