6373b7fb11b51f6fb8997e0f97d54fb96c98a7c9
Max Zheng <maxzheng.os@gmail.com>
2018-06-03 00:44:28 +0000

    Publish version 0.0.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='developer-tools',
-    version='0.0.10',
+    version='0.0.11',
 
     author='Max Zheng',
     author_email='maxzheng.os@gmail.com',
